7fa73e7539786f332d2d0df9f3d43066b5732b8f
BD00ley <dooley.r.bradley@gmail.com>
2020-11-23 12:40:51 -0500

    accidently only commited tb; see previous commit for details

--- src/SPI.v
+++ src/SPI.v
@@ -4,7 +4,7 @@ module spi(
     input clk,
     input [7:0] MOSI_data,
     input wire CS_n_i,
-    input [7:0] MISO_data,
+    output reg [7:0] MISO_data,
     output rdy,
 
     //SPI External ports
@@ -15,8 +15,7 @@ module spi(
 
 localparam idle = 2'b00;
 localparam load = 2'b01;
-localparam send = 2'b10;
-localparam done = 2'b11;
+localparam communicate = 2'b10;
 
 reg[1:0] state = idle;
 reg[2:0] counter = 0;
@@ -24,7 +23,7 @@ reg[2:0] counter = 0;
 reg [7:0] MOSI_buffer;
 reg [7:0] MISO_buffer;
 
-assign SCK = (CS_n == 1'b0 & state == send)  ? clk : 1'b0;
+assign SCK = (CS_n == 1'b0 & state == communicate)  ? clk : 1'b0;
 assign CS_n = CS_n_i;
 assign rdy = (state == idle) ? 1'b1 : 1'b0;
 assign MOSI = (state != idle) ? MOSI_buffer[7] : 1'b1;
@@ -37,28 +36,35 @@ end
 
 // Simple shift register 
 always @(posedge SCK) begin
-    if(state == send & counter != 3'b110)
+    MISO_buffer <= {MISO_buffer[6:0], MISO};
+    if(state == communicate & counter != 3'b110) begin
         MOSI_buffer <= {MOSI_buffer[6:0], 1'b1};
+    end
 end
 
 always @(posedge clk) begin
     if(state == idle) begin
+        counter <= 3'b000;
         if(CS_n == 1'b0)
             state <= load;
     end
     if(state == load) begin
         MOSI_buffer <= MOSI_data;
-        state <= send;
+        state <= communicate;
     end
-    if(state == send) begin
+    if(state == communicate) begin
         if(counter == 3'b110) begin
             counter <= 3'b000;
             if(CS_n == 1'b0) begin
                 MOSI_buffer <= MOSI_data;
-                state <= send;
+                state <= communicate ;
             end
             else
                 state <= idle;
+        end
+        else if (counter == 3'b000) begin
+            MISO_data <= MISO_buffer;
+            counter <= counter + 1;
         end
         else
             counter <= counter + 1;
